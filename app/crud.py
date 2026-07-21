749dc1841fe9f3cde5e4fd4b2f93e479401961d8
krishna_jaiswal <krishnajaiswal0984@gmail.com>
2026-07-21 12:04:37 +0530

    backend databse connected

---
+++ app/crud.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+
+from app import models
+from app import schemas
+from app.security import hash_password
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+
+    hashed_pwd = hash_password(user.password)
+
+    db_user = models.User(
+        username=user.username,
+        email=user.email,
+        password=hashed_pwd,
+        role="Engineer"
+    )
+
+    db.add(db_user)
+
+    db.commit()
+
+    db.refresh(db_user)
+
+    return db_user
+
+
+def get_user_by_username(db: Session, username: str):
+
+    return (
+        db.query(models.User)
+        .filter(models.User.username == username)
+        .first()
+    )
--- app/main.py
+++ app/main.py
@@ -1,1 +1,46 @@
- 
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import Base, engine, get_db
+from app import crud, schemas, models
+
+app = FastAPI(
+    title="DevOps Control Center",
+    version="1.0.0"
+)
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "DevOps Control Center API Running 🚀"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+@app.post("/register", response_model=schemas.UserResponse)
+def register(
+    user: schemas.UserCreate,
+    db: Session = Depends(get_db)
+):
+
+    existing_user = crud.get_user_by_username(
+        db,
+        user.username
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already exists"
+        )
+
+    return crud.create_user(db, user)
